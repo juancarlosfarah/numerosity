fae14c8b0984f87661dcdb5ed733646ad8949785
deRohrer <jonas.rohrer@epfl.ch>
2024-07-15 22:39:01 +0200

    feat: add basic numerosity task
    This version is not resolveable in the browser.

--- src/experiment.js
+++ src/experiment.js
@@ -1,18 +1,3 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.run = run;
 /**
  * @title Numerosity
  * @description Social numerosity task.
@@ -21,51 +6,97 @@ exports.run = run;
  * @assets assets/
  */
 // You can import stylesheets (.scss or .css).
-const plugin_fullscreen_1 = __importDefault(require("@jspsych/plugin-fullscreen"));
-const plugin_html_keyboard_response_1 = __importDefault(require("@jspsych/plugin-html-keyboard-response"));
-const plugin_preload_1 = __importDefault(require("@jspsych/plugin-preload"));
-const jspsych_1 = require("jspsych");
-require("../styles/main.scss");
+import FullscreenPlugin from '@jspsych/plugin-fullscreen';
+import jsPsychHtmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
+import PreloadPlugin from '@jspsych/plugin-preload';
+import jsPsychSurveyHtmlForm from '@jspsych/plugin-survey-html-form';
+import { initJsPsych } from 'jspsych';
+import '../styles/main.scss';
+const jsPsych = initJsPsych();
+function generateTimelineVars() {
+    const timeline_variables = [];
+    for (let num = 5; num <= 8; num++) {
+        for (let id = 0; id <= 9; id++) {
+            timeline_variables.push({ num, id });
+        }
+    }
+    return timeline_variables;
+}
+function partofexp(cntable, nb_trials = 20) {
+    const timeline_vars = generateTimelineVars();
+    let trials_done = 0;
+    const partofexp = {
+        timeline: [
+            {
+                type: jsPsychHtmlKeyboardResponse,
+                stimulus: '+',
+                choices: 'NO_KEYS',
+                trial_duration: 500,
+            },
+            {
+                type: jsPsychHtmlKeyboardResponse,
+                stimulus: function () {
+                    return `<img src='../assets/num_task_imgs/' + ${cntable} + 'num_' + ${jsPsych.timelineVariable('num')} + '_' + ${jsPsych.timelineVariable('id')}>`;
+                },
+                choices: 'NO_KEYS',
+                trial_duration: 2500,
+            },
+            {
+                type: jsPsychSurveyHtmlForm,
+                preamble: 'How many ' + cntable + ' were in the virtual room?',
+                html: '<input type="number" />',
+                on_finish: () => {
+                    trials_done++;
+                },
+            },
+        ],
+        timeline_variables: timeline_vars,
+        groups: [
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+            [10, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+            [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
+        ],
+        randomize_group_order: true,
+        sample: {
+            type: 'without-repetitions',
+            size: 5,
+        },
+        randomize_order: true,
+        loop_function: function () {
+            return trials_done < nb_trials;
+        },
+    };
+    return partofexp;
+}
 /**
  * This function will be executed by jsPsych Builder and is expected to run the jsPsych experiment
  *
  * @type {import("jspsych-builder").RunFunction}
  */
-function run(_a) {
-    return __awaiter(this, arguments, void 0, function* ({ assetPaths, input = {}, environment, title, version, }) {
-        const jsPsych = (0, jspsych_1.initJsPsych)();
-        const timeline = [];
-        // Preload assets
-        timeline.push({
-            type: plugin_preload_1.default,
-            images: assetPaths.images,
-            audio: assetPaths.audio,
-            video: assetPaths.video,
-        });
-        // Welcome screen
-        timeline.push({
-            type: plugin_html_keyboard_response_1.default,
-            choices: ['enter'],
-            stimulus: '<p>Welcome to Numerosity!<p/>',
-        });
-        timeline.push({
-            type: plugin_html_keyboard_response_1.default,
-            choices: ['a'],
-            stimulus: '<p>Page 2<p/>',
-        });
-        timeline.push({
-            type: plugin_html_keyboard_response_1.default,
-            choices: ['a'],
-            stimulus: '<p>Page 3<p/>',
-        });
-        // Switch to fullscreen
-        timeline.push({
-            type: plugin_fullscreen_1.default,
-            fullscreen_mode: true,
-        });
-        yield jsPsych.run(timeline);
-        // Return the jsPsych instance so jsPsych Builder can access the experiment results (remove this
-        // if you handle results yourself, be it here or in `on_finish()`)
-        return jsPsych;
+export async function run( /*{
+  assetPaths,
+  input = {},
+  environment,
+  title,
+  version,
+}*/) {
+    const timeline = [];
+    // Preload assets
+    timeline.push({
+        type: PreloadPlugin,
+        auto_preload: true,
     });
+    // Switch to fullscreen
+    timeline.push({
+        type: FullscreenPlugin,
+        fullscreen_mode: true,
+    });
+    const firsthalf = partofexp('people');
+    const secondhalf = partofexp('objects');
+    timeline.push(firsthalf, secondhalf);
+    await jsPsych.run(timeline);
+    // Return the jsPsych instance so jsPsych Builder can access the experiment results (remove this
+    // if you handle results yourself, be it here or in `on_finish()`)
+    return jsPsych;
 }
--- src/experiment.ts
+++ src/experiment.ts
@@ -7,53 +7,103 @@
  */
 // You can import stylesheets (.scss or .css).
 import FullscreenPlugin from '@jspsych/plugin-fullscreen';
-import HtmlKeyboardResponsePlugin from '@jspsych/plugin-html-keyboard-response';
+import jsPsychHtmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
 import PreloadPlugin from '@jspsych/plugin-preload';
+import jsPsychSurveyHtmlForm from '@jspsych/plugin-survey-html-form';
 import { JsPsych, initJsPsych } from 'jspsych';
 
 import '../styles/main.scss';
+
+const jsPsych: JsPsych = initJsPsych();
+
+type img_describtion = { num: number; id: number };
+type timeline = JsPsych['timeline'];
+
+function generateTimelineVars(): img_describtion[] {
+  const timeline_variables: img_describtion[] = [];
+
+  for (let num = 5; num <= 8; num++) {
+    for (let id = 0; id <= 9; id++) {
+      timeline_variables.push({ num, id });
+    }
+  }
+
+  return timeline_variables;
+}
+
+function partofexp(
+  cntable: 'people' | 'objects',
+  nb_trials: number = 20,
+): {
+  timeline: timeline;
+} {
+  const timeline_vars = generateTimelineVars();
+
+  let trials_done: number = 0;
+
+  const partofexp = {
+    timeline: [
+      {
+        type: jsPsychHtmlKeyboardResponse,
+        stimulus: '+',
+        choices: 'NO_KEYS',
+        trial_duration: 500,
+      },
+      {
+        type: jsPsychHtmlKeyboardResponse,
+        stimulus: function () {
+          return `<img src='../assets/num_task_imgs/' + ${cntable} + 'num_' + ${jsPsych.timelineVariable('num')} + '_' + ${jsPsych.timelineVariable('id')}>`;
+        },
+        choices: 'NO_KEYS',
+        trial_duration: 2500,
+      },
+      {
+        type: jsPsychSurveyHtmlForm,
+        preamble: 'How many ' + cntable + ' were in the virtual room?',
+        html: '<input type="number" />',
+        on_finish: () => {
+          trials_done++;
+        },
+      },
+    ],
+    timeline_variables: timeline_vars,
+    groups: [
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+      [10, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+      [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
+    ],
+    randomize_group_order: true,
+    sample: {
+      type: 'without-repetitions',
+      size: 5,
+    },
+    randomize_order: true,
+    loop_function: function (): boolean {
+      return trials_done < nb_trials;
+    },
+  };
+  return partofexp;
+}
 
 /**
  * This function will be executed by jsPsych Builder and is expected to run the jsPsych experiment
  *
  * @type {import("jspsych-builder").RunFunction}
  */
-export async function run({
+export async function run(/*{
   assetPaths,
   input = {},
   environment,
   title,
   version,
-}): Promise<JsPsych> {
-  const jsPsych: JsPsych = initJsPsych();
-
-  const timeline: JsPsych['timeline'] = [];
+}*/): Promise<JsPsych> {
+  const timeline: timeline = [];
 
   // Preload assets
   timeline.push({
     type: PreloadPlugin,
-    images: assetPaths.images,
-    audio: assetPaths.audio,
-    video: assetPaths.video,
-  });
-
-  // Welcome screen
-  timeline.push({
-    type: HtmlKeyboardResponsePlugin,
-    choices: ['enter'],
-    stimulus: '<p>Welcome to Numerosity!<p/>',
-  });
-
-  timeline.push({
-    type: HtmlKeyboardResponsePlugin,
-    choices: ['a'],
-    stimulus: '<p>Page 2<p/>',
-  });
-
-  timeline.push({
-    type: HtmlKeyboardResponsePlugin,
-    choices: ['a'],
-    stimulus: '<p>Page 3<p/>',
+    auto_preload: true,
   });
 
   // Switch to fullscreen
@@ -61,6 +111,11 @@ export async function run({
     type: FullscreenPlugin,
     fullscreen_mode: true,
   });
+
+  const firsthalf: timeline = partofexp('people');
+  const secondhalf: timeline = partofexp('objects');
+
+  timeline.push(firsthalf, secondhalf);
 
   await jsPsych.run(timeline);
 
